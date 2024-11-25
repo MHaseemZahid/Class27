44795895b89ad5f6532e37ed184141fa128e06b6
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-25 22:18:47 +0500

    Create function with return value

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-alert("ss");
+//  call_msg_console()// call function if it is created blow.
+// function call_msg_console(){
+//     console.log("call_msg_console123")
+// }
 
-function call_msg_console(){
-    console.log("call_msg_console")
+
+
+
+//create function with return value
+
+function fun_ret_val(){
+    var num1 =+prompt("Enter Your First Number");
+    var num2 =+prompt("Enter Your Second Number");
+    var output=num1+num2;
+    return  output;
 }
-call_msg_console()
+ var o1 =fun_ret_val();
+ console.log(o1);
