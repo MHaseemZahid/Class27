3ed8c05b18b82cdd4dd3844fc568c13dd791e302
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-25 22:34:34 +0500

    create function with return value and with parameters and arguments

--- app.js
+++ app.js
@@ -8,11 +8,32 @@
 
 //create function with return value
 
-function fun_ret_val(){
-    var num1 =+prompt("Enter Your First Number");
-    var num2 =+prompt("Enter Your Second Number");
-    var output=num1+num2;
-    return  output;
-}
- var o1 =fun_ret_val();
+// function fun_ret_val(){
+//     var num1 =+prompt("Enter Your First Number");
+//     var num2 =+prompt("Enter Your Second Number");
+//     var output=num1+num2;
+//     return  output;
+// }
+//  var o1 =fun_ret_val();
+//  console.log(o1);
+
+
+//create function with return value and with parameters and arguments
+
+
+ function fun_ret_val(num1,num2){//parameters 
+ 
+     var output=num1+num2;
+     return  output;
+ }
+ var o1 =fun_ret_val(100,300);// values  are arguments
+ var o2 =fun_ret_val(200,300);
+  
+  var o3 =fun_ret_val(300,300);
+ 
+  var o4 =fun_ret_val(400,300);
+  
  console.log(o1);
+ console.log(o2);
+ console.log(o3);
+  console.log(o4);
