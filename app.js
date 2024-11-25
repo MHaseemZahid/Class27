87c24743ad12517c595e324ba13b52f85f85ab94
MHaseemZahid <haseem.zahid@gmail.com>
2024-11-25 22:44:48 +0500

    create function to return first name and last name with parameter

--- app.js
+++ app.js
@@ -21,19 +21,32 @@
 //create function with return value and with parameters and arguments
 
 
- function fun_ret_val(num1,num2){//parameters 
+//  function fun_ret_val(num1,num2){//parameters 
  
-     var output=num1+num2;
-     return  output;
- }
- var o1 =fun_ret_val(100,300);// values  are arguments
- var o2 =fun_ret_val(200,300);
+//      var output=num1+num2;
+//      return  output;
+//  }
+//  var o1 =fun_ret_val(100,300);// values  are arguments
+//  var o2 =fun_ret_val(200,300);
   
-  var o3 =fun_ret_val(300,300);
+//   var o3 =fun_ret_val(300,300);
  
-  var o4 =fun_ret_val(400,300);
+//   var o4 =fun_ret_val(400,300);
   
- console.log(o1);
- console.log(o2);
- console.log(o3);
-  console.log(o4);
+//  console.log(o1);
+//  console.log(o2);
+//  console.log(o3);
+//  console.log(o4);
+
+
+// create function to return first name and last name with parameter 
+ function fun_fname_lname(fName,lName){//parameters 
+     var output=fName+lName;
+    return  output;
+}
+
+var o1 =fun_fname_lname("Muhammad ","Haseem");
+console.log(o1);
+ 
+
+
